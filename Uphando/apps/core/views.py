ee66f1d66565dd1c0042cc2fdbf1f807d990de75
Gerald Joshua <31729023+Geraldjoshua@users.noreply.github.com>
2022-09-15 17:30:42 +0200

    added a loginPage method

--- Uphando/apps/core/views.py
+++ Uphando/apps/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 from apps.job.models import Job
 
@@ -10,21 +11,28 @@ def frontpage(request):
 
     return render(request, 'core/frontpage.html', {'jobs': jobs})
 
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('job')
+        else:
+            messages.info(request, 'Username OR password is incorrect')
+
+    context = {}
+    return render(request, 'core/login.html', context)
+
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
 
         if form.is_valid():
             user = form.save()
-
-            # account_type = request.POST.get('account_type', 'jobseeker')
-            #
-            # if account_type == 'employer':
-            #     userprofile = Userprofile.objects.create(user=user, is_employer=True)
-            #     userprofile.save()
-            # else:
-            #     userprofile = Userprofile.objects.create(user=user)
-            #     userprofile.save()
 
             login(request, user)
 
