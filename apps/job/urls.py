0529a5c003dafce60c234dd7f20ea01c46790e69
Gerald Joshua <31729023+Geraldjoshua@users.noreply.github.com>
2022-12-07 03:28:08 +0200

    Update urls.py

--- apps/job/urls.py
+++ apps/job/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('search/', search, name='search'),
     path('add/', add_job, name='add_job'),
     path('<int:job_id>/', job_detail, name='job_detail'),
+    path('<int:job_id>/<int:job_id>/', job_detail, name='job_detail'),
     path('<int:job_id>/edit/', edit_job, name='edit_job'),
     path('<int:job_id>/delete/', delete_job, name='delete_job'),
 ]
